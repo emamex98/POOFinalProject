d50b6206207a114ba6aa53211c6022fb5ed73700
Emanuel Estrada Larios <emamex98@gmail.com>
2017-04-27 17:02:45 -0500

    added comments

--- AgendaDigital/src/AgendaLay.java
+++ AgendaDigital/src/AgendaLay.java
@@ -1,3 +1,11 @@
+/* Proyecto Final - Programacion Orientada a Objetos
+ * 
+ * Emanuel Estrada Larios - 50%
+ * Miguel Triana - 50%
+ * 
+ * jueves 27 de abril del 2017
+ */
+
 import javax.swing.JPanel;
 
 public class AgendaLay extends JPanel {
